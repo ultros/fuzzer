8af6223aea7cf78615e75b3a12c5faf1ec01c42f
Jesse Shelley <jesse@cybertutorials.org>
2022-06-30 00:37:28 -0600

    multithreading

--- fuzzer.py
+++ fuzzer.py
@@ -2,13 +2,13 @@ import requests
 import re
 import argparse
 import os
+import concurrent.futures
 from typing import TextIO, Tuple, Union
 
-
 def random_user_agent():
+    pass
 
-
-def format_url(url: str, keyword: str) -> Union[int, str]:
+def format_url(url: str, keyword: str) -> int | str:
     """Returns a formatted URL to fuzz."""
     try:
         if re.search("FUZZ", url):
@@ -48,28 +48,33 @@ def prepare_wordlist(url: str, wordlist: TextIO) -> Tuple[list, list, int]:
 
     return urls, badwords, total_urls
 
-def pp():
-    print("Processing...", end="\r")
 
+def process_url(url: str) -> str:
+    """Performs a GET request for the URL and returns a page status string."""
+    res = requests.get(url, timeout=10)
+    match res.status_code:
+        case 200:
+            return f"Discovered: {url}"
+        case 301:
+            return f"Permanent redirect: {url}"
+        case 302:
+            return f"Temporary redirect: {url}"
+        case _:
+            pass
 
 def fuzz(url: str, wordlist: TextIO) -> None:
     """Prints valid URLs from queries that resolve."""
     urls = prepare_wordlist(url, wordlist)
 
-    for url in urls[0]:
-        try:
-            req = requests.get(url, timeout=10)
-            match req.status_code:
-                case 200:
-                    print(f"Discovered: {url}")
-                case 301:
-                    print(f"Permanent redirect: {url}")
-                case 302:
-                    print(f"Temporary redirect: {url}")
-                case _:
-                    continue
-        except Exception as e:
-            print(f"***\n{e}\n***")
+    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+        futures = []
+
+        for url in urls[0]:
+                futures.append(executor.submit(process_url, url=url))
+
+        for future in concurrent.futures.as_completed(futures):
+            res = future.result()
+            print(res)
 
     print(f"Did not process: {urls[1]}")
 
@@ -87,6 +92,7 @@ def main() -> None:
 
     if args.url is not None:
         url = args.url
+
     if (args.wordlist is not None) and (os.path.isfile(args.wordlist)):
         wordlist = args.wordlist
     else:
